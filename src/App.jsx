982ad712a1f13b6d45b1d759adf4e785871a8d9d
desktop <mac@MacBook-Pro.local>
2024-04-16 15:46:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+/* eslint-disable react/no-unescaped-entities */
+const App = () => {
+  return (
+    <div className="header">
+      <img
+        height={70}
+        src="https://res.cloudinary.com/pitz/image/upload/v1711693779/WhatsApp_Image_2024-03-29_at_09.20.27_naxp5r.jpg"
+        alt=""
+      />
+
+      <div className="main">
+<h3 className="text" style={{color:'red',marginBottom:'2rem'}}>Community guidelines.</h3>
+
+<div>
+  <p>
+  1. Respect and Courtesy: Users must treat each other with respect and courtesy at all times. Any
+form of harassment, discrimination, or offensive behaviour will not be tolerated.
+<br />
+<br />
+<br />
+2. Authenticity: Users must provide accurate and truthful information about themselves.
+Misrepresentation of age, gender, interests, or any other personal details is strictly prohibited.
+<br />
+<br />
+<br />
+3. Privacy and Safety: Users should prioritize their own privacy and safety. They should refrain from
+sharing sensitive personal information, such as home addresses or financial details, with other users.
+
+Reporting any suspicious or inappropriate behaviour to the app administrators is encouraged.
+<br />
+<br />
+<br />
+4. Consent: Mutual consent is essential in all interactions. Users must respect each other's
+boundaries and refrain from engaging in any form of coercion or pressure.
+<br />
+<br />
+<br />
+5. No Illegal Activity: Users must not engage in any illegal activity or promote illegal behaviour
+through the app. This includes but is not limited to solicitation, drug use, or any form of criminal
+activity.
+<br />
+<br />
+<br />
+6. No Spam or Solicitation: Users must not use the app for spamming or solicitation purposes. Any
+form of unsolicited messages or advertisements is prohibited.
+<br />
+<br />
+<br />
+7. Responsible Communication: Users should communicate responsibly and refrain from using
+offensive language, sending unsolicited explicit content, or engaging in any form of cyberbullying.
+<br />
+<br />
+<br />
+8. Age Restrictions: Users must comply with the age restrictions set by the app. Minors should not
+use the app, and users must not engage in any inappropriate interactions with minors.
+<br />
+<br />
+<br />
+9. Feedback and Reporting: Users are encouraged to provide feedback to the app administrators
+regarding any issues or concerns they encounter. They should also report any violations of the app's
+rules and guidelines promptly.
+<br />
+<br />
+<br />
+10. Community Guidelines: Users must adhere to the specific community guidelines set by the app.
+These guidelines may include additional rules related to specific features or community standards.
+<br />
+<br />
+<br />
+11. Consequences for Violations: Violations of the app's rules and guidelines may result in
+disciplinary actions, including account suspension or termination, depending on the severity of the
+offense.
+<br />
+<br />
+<br />
+12. Updates and Modifications: The app reserves the right to update or modify the rules and
+guidelines as needed. Users will be notified of any changes, and it is their responsibility to stay
+informed and comply with the updated policies.
+<br />
+<br />
+<br />
+13.security: user security is paramount, the app will not be held responsible for any security issues,
+in case of any suspicious case report the account immediately for action to be taken
+
+
+
+  </p>
+</div>
+      </div>
+      <footer style={{
+  background:'black',
+  width:'100%',
+  textAlign:'center',
+  height:'2rem'
+}}>
+<p>These rules and guidelines aim to create a safe, respectful, and enjoyable environment for all users
+of the dating app.</p>
+</footer>
+    </div>
+  );
+};
+
+export default App;
